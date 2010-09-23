ac8dd990ef5ed31a84b34a5d94926ea72d44c8b7
Naoki Orii <mrorii@gmail.com>
2010-09-23 22:28:37 +0900

    added user following

--- spec/models/micropost_spec.rb
+++ spec/models/micropost_spec.rb
@@ -38,4 +38,33 @@ describe Micropost do
       @user.microposts.build(:content => 'a' * 141).should_not be_valid
     end
   end
+
+  describe 'from_users_followed_by' do 
+    before(:each) do 
+      @other_user = Factory(:user, :email => Factory.next(:email))
+      @third_user = Factory(:user, :email => Factory.next(:email))
+
+      @user_post  = @user.microposts.create!(:content => 'foo')
+      @other_post = @other_user.microposts.create!(:content => 'bar')
+      @third_post = @third_user.microposts.create!(:content => 'baz')
+
+      @user.follow!(@other_user)
+    end
+
+    it 'should have a from_users_followed_by class method' do 
+      Micropost.should respond_to(:from_users_followed_by)
+    end
+
+    it 'should include the followed users microposts' do 
+      Micropost.from_users_followed_by(@user).include?(@other_post).should be_true
+    end
+
+    it 'should inlucde the users own microposts' do 
+      Micropost.from_users_followed_by(@user).include?(@user_post).should be_true
+    end
+    
+    it 'should not inlucde an unfollowed users microposts' do
+      Micropost.from_users_followed_by(@user).include?(@third_user).should be_false
+    end
+  end
 end
